ab49fe4baf0ab76519a6a90378649715171935fd
ssimkani <seenasimkani@gmail.com>
2025-06-25 16:07:28 -0400

    Change name of validation set

--- split_data.py
+++ split_data.py
@@ -17,13 +17,13 @@ random.shuffle(data)
 # Split ratios
 n = len(data)
 train_end = int(0.8 * n)
-val_end   = train_end + int(0.1 * n)
+valid_end   = train_end + int(0.1 * n)
 
 # Write
 for name, subset in [
     ("data/train.jsonl", data[:train_end]),
-    ("data/val.jsonl",   data[train_end:val_end]),
-    ("data/test.jsonl",  data[val_end:])
+    ("data/valid.jsonl",   data[train_end:valid_end]),
+    ("data/test.jsonl",  data[valid_end:])
 ]:
     with open(name, "w") as out:
         for ex in subset:
